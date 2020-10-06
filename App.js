0563ada9e94327f2603dd1b1c8b5a1a63fb35c8b
Joel Roske <joel.j.roske@gmail.com>
2020-10-06 09:43:16 -0500

    input display

--- App.js
+++ App.js
@@ -5,36 +5,62 @@ import InputNumberButton from "./InputNumberButton";
 
 const buttons = [
   ["CLR", "DEL"],
-  [7, 8, 9, "/"],
-  [4, 5, 6, "x"],
-  [1, 2, 3, "-"],
-  [0, ".", "=", "+"],
+  ["7", "8", "9", "/"],
+  ["4", "5", "6", "x"],
+  ["1", "2", "3", "-"],
+  ["0", ".", "=", "+"],
 ];
 
 export default class App extends Component {
-
+  state = {
+    displayValue: 0,
+  };
   renderButtons() {
     let layouts = buttons.map((buttonRows, index) => {
       let rowItem = buttonRows.map((buttonItems, buttonIndex) => {
-        return <InputNumberButton 
-        value={buttonItems}
-        handleOnPress={() =>{}}
-        key={'btn-' + buttonIndex} />
-      })
+        return (
+          <InputNumberButton
+            value={buttonItems}
+            handleOnPress={this.handleInput}
+            key={"btn-" + buttonIndex}
+          />
+        );
+      });
 
-      return <View style={styles.inputRow} key={'row-' + index}>
+      return (
+        <View style={styles.inputRow} key={"row-" + index}>
           {rowItem}
         </View>
-    })
+      );
+    });
     return layouts;
   }
+
+  handleInput = (input) => {
+    const { displayValue } = this.state;
+
+    // switch (key) {
+    //   case value:
+    //     break;
+
+    //   default:
+    //     break;
+    // }
+    this.setState({
+      displayValue: displayValue === "0" ? input : displayValue + input,
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.resultContainer}>
-          <Text style={styles.resultText}>0</Text>
+          <Text style={styles.resultText}>
+            {this.state.displayValue}
+          </Text>
         </View>
         <View style={styles.inputContainer}>{this.renderButtons()}</View>
+        <View>History</View>
       </View>
     );
   }
@@ -54,14 +80,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#ee2e23",
   },
   resultText: {
-    color: 'white',
+    color: "white",
     fontSize: 80,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     padding: 20,
-    textAlign: 'right',
+    textAlign: "right",
   },
   inputRow: {
     flex: 1,
-    flexDirection: 'row',
+    flexDirection: "row",
   },
 });
--- inputNumberButton.js
+++ inputNumberButton.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     margin: 1,
-    backgroundColor: 'rgba(255, 255, 255, 0.1',
+    backgroundColor: 'red',
     justifyContent: 'center',
     alignItems: 'center'
   },
